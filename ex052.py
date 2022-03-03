0f5a933bb94808c561d1a8842d7ba61fa4bcdfe1
Paula Jardim <paulajferreira@yahoo.com.br>
2022-03-02 22:27:03 -0300

    estrutura de repetição

--- ex052.py
+++ ex052.py
@@ -1,4 +1,18 @@
 #número primo
 num = int(input('Digite um número: '))
+total = 0
 
 for c in range (1, num +1):
+    if num % c == 0:
+        print('\033[34m', end=" ")
+        total += 1
+    else:
+        print('\033[m', end=" ")
+    print('{}'.format(c), end=" ")
+
+print('\n\033[mO número {} foi divisível por {} vezes.'.format(num, total))
+
+if total == 2:
+    print('Por isso ele é primo.')
+else:
+    print('Por isso ele não é primo.')
--- ex053.py
+++ ex053.py
@@ -1,5 +1,19 @@
 #palíndromo
 
 print('Verificador de Palíndromo')
-frase = str(input('Insira a frase e/ou palavra: ')).strip()
-palindromo = 0
+frase = str(input('Insira a frase e/ou palavra: ')).strip().upper()
+letras = frase.split()
+junto = ''.join(letras)
+
+print('Você digitou a frase {}'.format(junto))
+
+inverso = ''
+
+for letra in range (len(junto) - 1, -1, -1):
+    inverso += junto [letra]
+print(junto, inverso)
+
+if inverso == junto:
+    print('É um palíndromo')
+else:
+    print('Não é um palíndromo')
