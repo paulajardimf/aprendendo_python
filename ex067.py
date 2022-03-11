942a2fa432e03a2199f82fa4b2282940cd3ddf7b
Paula Jardim <paulajferreira@yahoo.com.br>
2022-03-10 22:39:35 -0300

    estrutura de repetição while com break

--- ex067.py
+++ ex067.py
@@ -2,7 +2,8 @@
 
 while True:
     num = (int(input('Insira um número para visualizar a sua tabuada: ')))
-    for c in range(1,11):
-        print(f'{c:2} X {num} = {c * num}')
     if num < 0:
         break
+    for c in range(1, 11):
+        print(f'{c:2} X {num} = {c * num}')
+print('Programa encerrado.')
--- ex068.py
+++ ex068.py
@@ -1,1 +1,22 @@
 #par ou impar
+from random import randint
+
+print('---JOGO PAR OU IMPAR---\n')
+
+while True:
+    num_maq = randint(0, 5)
+    escolha = str(input('Par ou Impar? [ Digite P ou I ]: ')).upper().split()
+    num_pessoa = int (input('Digite um número de 0 a 5: '))
+    par_impar = (num_maq + num_pessoa) % 2
+
+    if par_impar == 0 and escolha == 'P':
+        print(f'O Computador escolheu o número {num_maq}')
+        print('VOCÊ GANHOU!!!')
+        break
+    elif par_impar != 0 and escolha == 'I':
+        print(f'O Computador escolheu o número {num_maq}')
+        print('VOCÊ GANHOU!!!')
+        break
+    else:
+        print(f'O Computador escolheu o número {num_maq}')
+        print('Tente novamente.')
